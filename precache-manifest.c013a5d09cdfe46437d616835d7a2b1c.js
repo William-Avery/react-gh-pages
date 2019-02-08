2cf0fa4eb225717c4ff1bc1c0dc585fb04bdef3d
William Avery <willaveryvy@gmail.com>
2019-02-07 21:50:11 -0500

    Updates

---
+++ precache-manifest.c013a5d09cdfe46437d616835d7a2b1c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bd4e3fb9c953b16ed71",
+    "url": "/William-Avery/react-gh-pages/static/js/runtime~main.9bd4e3fb.js"
+  },
+  {
+    "revision": "98c08fc4086b0e0ef67a",
+    "url": "/William-Avery/react-gh-pages/static/js/main.98c08fc4.chunk.js"
+  },
+  {
+    "revision": "ca1b841b83730835dd92",
+    "url": "/William-Avery/react-gh-pages/static/js/1.ca1b841b.chunk.js"
+  },
+  {
+    "revision": "9feacb9ca013b1928b2e92d5b75cdb66",
+    "url": "/William-Avery/react-gh-pages/index.html"
+  }
+];
